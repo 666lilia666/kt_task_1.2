d108412694c380e46aebd98759700a9a9596b863
KMS-01 <666lilia666@gmail.com>
2022-10-04 16:35:36 +0300

    refactoring Task_1

--- src/main/kotlin/Task_1.kt
+++ src/main/kotlin/Task_1.kt
@@ -1,10 +1,9 @@
-fun main (){
+fun main() {
     val currency = "руб."
     val amount = 10_152
     val commission = 0.75
     val minCommission = 35
     val sumCommission = (amount * commission) / 100
-    val transferCommission = if (sumCommission > minCommission) {
-        println("Сумма комиссии $sumCommission $currency")} else { println("Сумма комиссии $minCommission $currency") }
-
+    val transferCommission = if (sumCommission > minCommission) sumCommission else minCommission
+    println("Сумма комиссии $transferCommission $currency")
 }
