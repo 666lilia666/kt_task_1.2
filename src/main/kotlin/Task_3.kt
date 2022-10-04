219b8494dc28158ac6b167ec9e9b9c314ef92db2
KMS-01 <666lilia666@gmail.com>
2022-10-04 17:19:40 +0300

    some work Task_3

---
+++ src/main/kotlin/Task_3.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val musicLovers = false
+    val shopAmount = 13_000
+    val standardDiscount = 100
+    val discountStageOneMinValue = 1_001
+    val discountStageOneMaxValue = 10_000
+    val discountStageTwoValue = 10_001
+    val whoNou = (shopAmount * 5) / 100
+    val discount = if (discountStageOneMinValue <= shopAmount && shopAmount <= discountStageOneMaxValue)
+        shopAmount - standardDiscount else if (shopAmount >= discountStageTwoValue) whoNou else shopAmount
+    var result = shopAmount - discount
+    if (musicLovers) result -= (result * 1) / 100
+    println(result)
+}
