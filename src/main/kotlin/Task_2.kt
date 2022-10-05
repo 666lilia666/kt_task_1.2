8057ea6762b23cd16c96e207cd42749f5ecb38ad
666lilia666 <107703727+666lilia666@users.noreply.github.com>
2022-10-05 13:34:07 +0300

    Update Task_2.kt

--- src/main/kotlin/Task_2.kt
+++ src/main/kotlin/Task_2.kt
@@ -2,7 +2,6 @@ fun main() {
     val likes: Int = 800
     val variableOne = "людям"
     val variableTwo = "человеку"
-    println(likes % 2)
     val result =
         if (likes % 2 === 0) variableOne else if (!(likes % 2 === 0) && likes % 3 === 0) variableOne else variableTwo
     println("Понравилось $likes $result")
